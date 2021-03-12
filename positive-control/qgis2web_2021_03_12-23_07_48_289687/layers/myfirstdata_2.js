701f98cbaf6369929a460f42b1335ce781892e2e
Haskan Kaya <49439886+haskankaya@users.noreply.github.com>
2021-03-12 23:14:11 +0000

    Delete positive-control/qgis2web_2021_03_12-23_07_48_289687 directory

--- positive-control/qgis2web_2021_03_12-23_07_48_289687/layers/myfirstdata_2.js
+++
@@ -1,1 +0,0 @@
-var json_myfirstdata_2 = {"type":"FeatureCollection","name":"myfirstdata_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","label":null,"NameLabel":"Buckingham Palace"},"geometry":{"type":"Point","coordinates":[-0.140092175680881,51.502035451855122]}}]}
